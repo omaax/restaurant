bfd6422816f28ed54189e9364b54d9a777d6bf84
Your NameOmar <omvxvr@gmail.com>
2026-02-28 19:20:37 +0200

    reviews section is done + about and home page are responsive

--- components/home/Ingredients.tsx
+++ components/home/Ingredients.tsx
@@ -2,13 +2,14 @@
 
 const Ingredients = () => {
   return (
-    <div className='flex flex-col sm:flex-row bg-custom-gray'>
-        <div className='flex flex-col flex-1 items-center justify-center ml-0 sm:ml-60'>
+    <div className='flex flex-col md:flex-row bg-custom-gray'>
+        <div className='flex flex-col flex-1 items-center justify-center ml-0 sm:mx-10 lg:ml-60'>
                 <h1 className='text-4xl m-3 text-center p-2 sm:m-4 sm:p-5'>Ingredients & Method</h1>
-                <p className='text-center mb-10 px-10 sm:px-30'>Flour, water, sea salt, and leavening: four simple ingredients that—when mixed, shaped, fermented, and baked—make one of the most basic foods that has nourished humans for thousands of years. Since such few ingredients are involved, two things set great bread apart: quality ingredients and time.</p>
+                <p className='text-center mb-10 sm:px-2 '>Flour, water, sea salt, and leavening: four simple ingredients that—when mixed, shaped, fermented, and baked—make one of the most basic foods that has nourished humans for thousands of years. Since such few ingredients are involved, two things set great bread apart: quality ingredients and time.</p>
             <button className='mb-10 bg-transparent hover:bg-black hover:text-white rounded-full border border-black font-bold h-[55px] w-[200px]'>READ MORE</button>
         </div>
-        <div className='bg-[url(/bread-bg.jpg)] h-100 w-100 sm:h-[653px] sm:w-[860px] bg-center bg-cover' />
+        {/* <div className='bg-[url(/bread-bg.jpg)] h-100 w-100 sm:h-[653px] sm:w-[860px] bg-center bg-cover' /> */}
+        <div className='bg-[url(/bread-bg.jpg)] h-100 w-fill sm:h-150 md:h-fill md:w-full lg:h-[653px] lg:w-[860px] bg-center bg-cover' />
     </div>
   )
 }
--- components/home/Reviews.tsx
+++ components/home/Reviews.tsx
@@ -3,40 +3,38 @@ import Image from "next/image"
 
 const Reviews = () => {
   return (
-    <div className='relative'>
-      <div className="h-[750px]">
-        <Image className="absolute h-[750px] w-full" src="/reviews.jpg" alt="" width={750} height={750}></Image>
-        <div className="absolute text-white z-10">
-          <h1>Read what our customers say</h1>
-          <div className="grid grid-cols-4">
-            <div className="bg-[#f2952d] h-70 w-60 m-10">
-              <div className="bg-white top-10 left-15 absolute flex items-center justify-center size-16 rounded-full ">
-                <MessageSquareQuote className="text-[#f2952d] " />
-              </div>
-              <p className="italic font-sans pt-15 px-5">Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Ut sem nulla pharetra diam sit amet nisl. </p>
-              <h1 className="font-bold text-xl uppercase px-5 pt-5 font-sans">Celia Almeda</h1>
+    <div className="relative h-[1600px] sm:h-[1500px] lg:h-[750px]">
+      <Image className="absolute h-full -z-10 w-full" src="/reviews.jpg" alt="" width={750} height={750} />
+      <div className=" absolute inset-0 flex flex-col items-center justify-center gap-16 text-white">
+        <h1 className="text-center text-4xl sm:text-6xl text-white">Read what our customers say</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+          <div className="bg-[#f2952d] relative h-70 w-60">
+            <div className="bg-white -top-5 left-5 absolute flex items-center justify-center size-16 rounded-full ">
+              <MessageSquareQuote className="text-[#f2952d] " />
             </div>
-            <div className="bg-[#f2952d] h-70 w-60 m-10">
-              <div className="bg-white top-10 left-15 absolute flex items-center justify-center size-16 rounded-full ">
-                <MessageSquareQuote className="text-[#f2952d] " />
-              </div>
-              <p className="italic font-sans pt-15 px-5">Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Ut sem nulla pharetra diam sit amet nisl. </p>
-              <h1 className="font-bold text-xl uppercase px-5 pt-5 font-sans">Celia Almeda</h1>
+            <p className="italic font-sans pt-15 px-5">Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Ut sem nulla pharetra diam sit amet nisl. </p>
+            <h1 className="font-bold text-xl uppercase px-5 pt-5 font-sans">Celia Almeda</h1>
+          </div>
+          <div className="bg-[#f2952d] relative h-70 w-60">
+            <div className="bg-white -top-5 left-5 absolute flex items-center justify-center size-16 rounded-full ">
+              <MessageSquareQuote className="text-[#f2952d] " />
             </div>
-            <div className="bg-[#f2952d] h-70 w-60 m-10">
-              <div className="bg-white top-10 left-15 absolute flex items-center justify-center size-16 rounded-full ">
-                <MessageSquareQuote className="text-[#f2952d] " />
-              </div>
-              <p className="italic font-sans pt-15 px-5">Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Ut sem nulla pharetra diam sit amet nisl. </p>
-              <h1 className="font-bold text-xl uppercase px-5 pt-5 font-sans">Celia Almeda</h1>
+            <p className="italic font-sans pt-15 px-5">Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Ut sem nulla pharetra diam sit amet nisl. </p>
+            <h1 className="font-bold text-xl uppercase px-5 pt-5 font-sans">Celia Almeda</h1>
+          </div>
+          <div className="bg-[#f2952d] relative h-70 w-60">
+            <div className="bg-white -top-5 left-5 absolute flex items-center justify-center size-16 rounded-full ">
+              <MessageSquareQuote className="text-[#f2952d] " />
             </div>
-            <div className="bg-[#f2952d] h-70 w-60 m-10">
-              <div className="bg-white top-10 left-15 absolute flex items-center justify-center size-16 rounded-full ">
-                <MessageSquareQuote className="text-[#f2952d] " />
-              </div>
-              <p className="italic font-sans pt-15 px-5">Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Ut sem nulla pharetra diam sit amet nisl. </p>
-              <h1 className="font-bold text-xl uppercase px-5 pt-5 font-sans">Celia Almeda</h1>
+            <p className="italic font-sans pt-15 px-5">Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Ut sem nulla pharetra diam sit amet nisl. </p>
+            <h1 className="font-bold text-xl uppercase px-5 pt-5 font-sans">Celia Almeda</h1>
+          </div>
+          <div className="bg-[#f2952d] relative h-70 w-60">
+            <div className="bg-white -top-5 left-5 absolute flex items-center justify-center size-16 rounded-full ">
+              <MessageSquareQuote className="text-[#f2952d] " />
             </div>
+            <p className="italic font-sans pt-15 px-5">Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Ut sem nulla pharetra diam sit amet nisl. </p>
+            <h1 className="font-bold text-xl uppercase px-5 pt-5 font-sans">Celia Almeda</h1>
           </div>
         </div>
       </div>
