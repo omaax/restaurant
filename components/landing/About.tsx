178290641ae63a5c30fe67398c24d63bc5d54f5c
Omar <omvxvr@gmail.com>
2026-03-01 11:24:47 +0200

    commit

--- components/landing/About.tsx
+++ components/landing/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className='bg-[#e5e5e5]'>
         <div className="flex flex-col md:flex-row items-center justify-center pt-70 pb-30">
             {/* TEXT */}
-            <div className="bg-white h-200 sm:h-150 w-150 px-[50px] py-[30px]">
+            <div className="bg-white h-200 sm:h-150 w-80 mt-10 md:mt-0 md:w-150 px-[50px] py-[30px]">
                 <h1 className="text-xl sm:text-6xl">About Bakery</h1>
                 <p className=" pt-10 text-sm sm:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-15 pt-10 text-xl uppercase">
@@ -20,7 +20,7 @@ const About = () => {
             </div>
             {/* PHOTO */}
             <div>
-                <Image className="h-150 w-125" src="/about2.jpg" alt="" height={500} width={500}/>
+                <Image className="h-100 w-80 md:h-150 md:w-125" src="/about2.jpg" alt="" height={500} width={500}/>
             </div>
         </div>
     </div>
--- components/landing/Mission2.tsx
+++ components/landing/Mission2.tsx
@@ -4,8 +4,8 @@ const Mission2 = () => {
   return (
     <div className='bg-[#e5e5e5]'>
         <div className="h-[600px] relative">
-        <Image className="h-[700px] w-[1000px] absolute m-auto mr-100 left-0 right-0" width={800} height={800} src="/mission2.jpg" alt="mission" />
-          <div className="w-[590px] h-[440px] bg-white  shadow-2xl shadow-gray-900 top-140 left-90 absolute">
+        <Image className="h-70 w-80 md:h-[650px] md:w-[1000px] absolute m-10 mr-100 left-0 md:left-[20%]" width={800} height={800} src="/mission2.jpg" alt="mission" />
+          <div className="w-[300px] md:w-[590px] h-[500xp] md:h-[440px] bg-white shadow-2xl shadow-gray-900 top-[60%] md:top-[80%] left-[10%] absolute">
               <div className="m-10">
                 <h1 className="text-4xl mb-4">Our Mission</h1>
                 <p className="text-xl mb-4">We opened our first bakery in the South End in 2000 with one goal: to make the world a sweeter place through warm smiles, gooey sticky buns, in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
