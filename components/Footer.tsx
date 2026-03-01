21adab669391f92ad5dc60c52f0fcc6546e0666c
Omar <omvxvr@gmail.com>
2026-03-01 11:53:46 +0200

    commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
     <div className='h-[300px] bg-custom-black text-white flex flex-col gap-20 items-center justify-center'>
       <p>Sample text. Click to select the Text Element.</p>
       <p className="text-sm">
-        This site was created with the <span className="text-yellow-600 underline cursor-pointer hover:-underline-offset-1">Nicepage</span>
+        This site was created with the <span className="text-yellow-600 underline cursor-pointer hover:-underline-offset-2">Omar</span>
       </p>
     </div>
   )
